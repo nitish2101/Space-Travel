58ccfe7072a11c0c9f32671cad878547873a39a9
Nitish Kumar Vashisht <71669388+nitish2101@users.noreply.github.com>
2021-11-18 23:51:38 +0530

    Add files via upload

---
+++ crew.js
@@ -0,0 +1,32 @@
+function mark() {
+  document.querySelector(".post").innerHTML = "MISSION SPECIALIST";
+  document.querySelector(".name").innerHTML = "MARK SHUTTLEWORTH";
+  document.querySelector("#post-info").innerHTML =
+    "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.";
+  document.querySelector(".portrait").src =
+    "./assets/crew/image-mark-shuttleworth.png";
+}
+function victor() {
+  document.querySelector(".post").innerHTML = "PILOT";
+  document.querySelector(".name").innerHTML = "VICTOR GLOVER";
+  document.querySelector("#post-info").innerHTML =
+    "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ";
+  document.querySelector(".portrait").src =
+    "./assets/crew/image-victor-glover.png";
+}
+function anousheh() {
+  document.querySelector(".post").innerHTML = "FLIGHT ENGINNER";
+  document.querySelector(".name").innerHTML = "ANOUSHEH ANSARI";
+  document.querySelector("#post-info").innerHTML =
+    "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. ";
+  document.querySelector(".portrait").src =
+    "./assets/crew/image-anousheh-ansari.png";
+}
+function douglas() {
+  document.querySelector(".post").innerHTML = "COMMANDER";
+  document.querySelector(".name").innerHTML = "DOUGLAS HURLEY";
+  document.querySelector("#post-info").innerHTML =
+    "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.";
+  document.querySelector(".portrait").src =
+    "./assets/crew/image-douglas-hurley.png";
+}
---
+++ destination.js
@@ -0,0 +1,39 @@
+function moonToMars() {
+  document.querySelector(".mars").classList.add("pressed");
+  setTimeout(function () {
+    document.querySelector(".mars").classList.remove("pressed");
+  }, 300);
+  document.querySelector(".moonImg").src =
+    "./assets/destination/image-mars.png";
+  document.querySelector(".planet-info").innerHTML =
+    "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!";
+  document.querySelector(".planet-heading").innerHTML = "MARS";
+}
+function moonToEuropa() {
+  document.querySelector(".europa").classList.add("pressed");
+  setTimeout(function () {
+    document.querySelector(".europa").classList.remove("pressed");
+  }, 300);
+  document.querySelector(".moonImg").src =
+    "./assets/destination/image-europa.png";
+  document.querySelector(".planet-info").innerHTML =
+    "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.";
+  document.querySelector(".planet-heading").innerHTML = "EUROPA";
+}
+function moonToTitan() {
+  document.querySelector(".titan").classList.add("pressed");
+  setTimeout(function () {
+    document.querySelector(".titan").classList.remove("pressed");
+  }, 300);
+  document.querySelector(".moonImg").src =
+    "./assets/destination/image-titan.png";
+  document.querySelector(".planet-info").innerHTML =
+    "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.";
+  document.querySelector(".planet-heading").innerHTML = "TITAN";
+}
+function moon() {
+  document.querySelector(".moon").classList.add("pressed");
+  setTimeout(function () {
+    document.querySelector(".moon").classList.remove("pressed");
+  }, 300);
+}
